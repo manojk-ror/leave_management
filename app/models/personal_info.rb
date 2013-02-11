e5ff13bf13f8431078b6690ba4e1c28aacb71f86
manoj <manoj@grepruby.com>
2013-02-11 15:31:56 +0530

    remove one column as leave_info _id and configure also

--- app/models/personal_info.rb
+++ app/models/personal_info.rb
@@ -1,4 +1,4 @@
 class PersonalInfo < ActiveRecord::Base
   attr_accessible :alternate_email, :date_of_birth, :email, :employee_id, :father_name, :home_phone, :local_address, :mother_name, :name, :permanent_address, :phone
-belongs_to :employee
+  belongs_to :employee
 end
--- db/migrate/20130208150157_create_salaries.rb
+++ db/migrate/20130208150157_create_salaries.rb
@@ -1,9 +1,8 @@
 class CreateSalaries < ActiveRecord::Migration
   def change
     create_table :salaries do |t|
-      t.integer :leave_info_id
       t.integer :employee_id
-      t.date :applicable_month
+      t.datetime :applicable_month
       t.float :previous_month_salary
       t.float :applicable_salary
       t.float :deduction
