5440bf097c1673b9791d723ae8d3597726dc50c8
manoj <manoj@grepruby.com>
2013-02-16 20:10:58 +0530

    admin record

--- db/migrate/20130214063306_add_admin_status_to_employee.rb
+++ db/migrate/20130214063306_add_admin_status_to_employee.rb
@@ -1,9 +1,7 @@
 class AddAdminStatusToEmployee < ActiveRecord::Migration
   def change
     add_column :employees, :admin_status, :boolean, :default => false
-    employee = Employee.create!(:email => 'admin@grepruby.com', :password => 'grepruby', :admin_status => true)
-      #employee.skip_confirmation!
-      employee.save
+    employee = Employee.create(:email => 'admin@grepruby.com', :password => 'grepruby', :admin_status => true)
     end  
   end
 end
