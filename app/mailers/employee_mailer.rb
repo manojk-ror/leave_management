dc950e9c48015ffe73ec14d6dfdea9e582fe2e17
manoj <manoj@grepruby.com>
2013-02-26 21:22:23 +0530

    generate employee_mailer

---
+++ app/mailers/employee_mailer.rb
@@ -0,0 +1,10 @@
+class EmployeeMailer < ActionMailer::Base
+  default from: "leave_notification@grepruby.com"
+  
+  def welcome_email(leave_info)
+    @leave_info = leave_info
+    mail(:to => @leave_info.to_email_ids, :subject => "Application for Leave")
+    mail(:cc => @leave_info.to_email_ids, :subject => "Inform of Leave")
+  end
+
+end
