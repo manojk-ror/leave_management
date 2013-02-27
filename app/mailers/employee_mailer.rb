913486ad9e5c6641e021ae11efd23a60ee4dd80c
manoj <manoj@grepruby.com>
2013-02-27 12:49:15 +0530

    change to in teo time

--- app/mailers/employee_mailer.rb
+++ app/mailers/employee_mailer.rb
@@ -4,7 +4,7 @@ class EmployeeMailer < ActionMailer::Base
   def welcome_email(leave_info)
     @leave_info = leave_info
     mail(:to => @leave_info.to_email_ids, :subject => "Application for Leave")
-    mail(:cc => @leave_info.to_email_ids, :subject => "Inform of Leave")
+    mail(:cc => @leave_info.cc_email_ids, :subject => "Inform of Leave")
   end
 
 end
