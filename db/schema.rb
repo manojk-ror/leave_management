41be025686164d4df6797e0dd80fdd84cb6e97d5
manoj <manoj@grepruby.com>
2013-03-04 18:29:27 +0530

    db/schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130304092816) do
+ActiveRecord::Schema.define(:version => 20130304125423) do
 
   create_table "employees", :force => true do |t|
     t.string   "email",                  :default => "", :null => false
